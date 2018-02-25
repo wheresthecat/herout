3a038e4dd964daaec7014147c55f955283747a10
Honza Kováč <frosty@centrum.cz>
2018-02-25 21:39:04 +0100

    ->

--- cv0304.c
+++ cv0304.c
@@ -18,11 +18,15 @@ int main(int argc, char const *argv[])
 	fileRead = fopen("PISMENA.TXT", "r");
 	fileWrite = fopen("KOLIK.TXT", "ab+");
 
+	
+	// Cyklus bezi do konce souboru.
 	while(c != EOF)
 	{
 		count++;
-		if((c = getc(fileRead)) == '\n')
+		if((c = getc(fileRead)) == '\n') // Podminka hlida konec radku.
 		{
+			// -1 je kvuli pricteni noveho radku jako znaku.
+			wholeCount += count - 1;
 			printf("V radku bylo %d znaku.\n", count - 1 );
 			fprintf(fileWrite, "V radku bylo %d znaku.\n", count - 1);
 			count = 0;
---
+++ cv0305.c
@@ -0,0 +1,28 @@
+/**
+Napište program, který na každý řádek vypíše násobek čísla 3.14
+**/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define PI 3.14
+
+int main(int argc, char const *argv[])
+{
+	FILE *fileWrite;
+
+	if((fileWrite = fopen("CISLA.TXT", "ab+")) == NULL)
+	{
+		printf("Soubor se nepodarilo otevrit.\n");
+		return 1;
+	}
+
+	for(int i = 1; i <= 20; i++)
+	{
+		double output = (double)PI * i;
+		fprintf(fileWrite, "%.2lf\n", output);
+	}
+	
+
+	return 0;
+}
